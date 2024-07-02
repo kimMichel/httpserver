a5c4e871bcb95eb9c17a5c91b77ad5a8a445c242
Michel Kim <kim98.michel@gmail.com>
2024-07-02 00:08:23 -0300

    make it pass

--- server.go
+++ server.go
@@ -16,7 +16,7 @@ type PlayerServer struct {
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, GetPlayerScore(player))
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
 
 func GetPlayerScore(name string) string {
