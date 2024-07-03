076c31caf3a663165383422ba6f488da384b82b5
Michel Kim <kim98.michel@gmail.com>
2024-07-03 16:14:10 -0300

    refactor

--- server.go
+++ server.go
@@ -37,13 +37,15 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
-	leagueTable := []Player{
-		{"Michel", 20},
-	}
-
-	json.NewEncoder(w).Encode(leagueTable)
+	json.NewEncoder(w).Encode(p.getLeagueTable())
 
 	w.WriteHeader(http.StatusOK)
+}
+
+func (p *PlayerServer) getLeagueTable() []Player {
+	return []Player{
+		{"Michel", 20},
+	}
 }
 
 func (p *PlayerServer) playersHandler(w http.ResponseWriter, r *http.Request) {
